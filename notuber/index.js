ad06b9b7f7174b8ae0e39beb0f1ee0f72c61644a
hollyhleal <hollyhleal@gmail.com>
2022-10-19 18:35:34 -0600

    commented out lines 38-43 for testing

--- notuber/index.js
+++ notuber/index.js
@@ -35,9 +35,9 @@ script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBhXJr5eCkwhUUWGi
 script.async = true;
 
 // Attach your callback function to the `window` object
-window.initMap = function() {
+//window.initMap = function() {
   // JS API is loaded and available
-};
+//};
 
 // Append the 'script' element to 'head'
-document.head.appendChild(script);
+//document.head.appendChild(script);
