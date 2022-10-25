2712cc794c1118e391dc338e246c5bc58f37a33d
hollyhleal <hollyhleal@gmail.com>
2022-10-24 21:55:31 -0600

    attempted alternate way to add markers

--- notuber/index.js
+++ notuber/index.js
@@ -2,7 +2,7 @@ var map;
 var marker;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.352271, lng: -71.05524200000001 },
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -10,14 +10,25 @@ function initMap() {
   setMarkers(map);
 }
 
-//const cars: [String, Number, Number, Number][] = [
-  //["mXfkjrFw", 42.3453, -71.0464, 1],
-  //["nZXB8ZHz", 42.3662, -71.0621, 2],
-  //["Tkwu74WC", 42.3603, -71.0547, 3],
-  //["5KWpnAJN", 42.3472, -71.0802, 4],
-  //["uf5ZrXYw", 42.3663, -71.0544, 5],
-  //["VMerzMH8", 42.3542, -71.0704, 6],
-//];
+var cars = [
+  ["mXfkjrFw", 42.3453, -71.0464, 1],
+  ["nZXB8ZHz", 42.3662, -71.0621, 2],
+  ["Tkwu74WC", 42.3603, -71.0547, 3],
+  ["5KWpnAJN", 42.3472, -71.0802, 4],
+  ["uf5ZrXYw", 42.3663, -71.0544, 5],
+  ["VMerzMH8", 42.3542, -71.0704, 6],
+];
+
+/*var marker, i;
+
+for (i = 0; i <cars.length; i++) {
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(cars[i][1], cars[i][2], cars[i][3], cars[i][4], cars[i][5], cars[i][6]),
+    map: map,
+    icon: image,
+  })
+  const image = "notuber/car.png";
+}*/
 
 function setMarkers(map) {
   const image = "notuber/car.png";
