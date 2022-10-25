851914660b241315034c4e5d3d95f611ed7a5c46
hollyhleal <hollyhleal@gmail.com>
2022-10-24 21:25:43 -0600

    reordered elements, hid const cars for testing

--- notuber/index.js
+++ notuber/index.js
@@ -10,28 +10,25 @@ function initMap() {
   setMarkers(map);
 }
 
-const cars = [
-  ["mXfkjrFw", 42.3453, -71.0464, 1],
-  ["nZXB8ZHz", 42.3662, -71.0621, 2],
-  ["Tkwu74WC", 42.3603, -71.0547, 3],
-  ["5KWpnAJN", 42.3472, -71.0802, 4],
-  ["uf5ZrXYw", 42.3663, -71.0544, 5],
-  ["VMerzMH8", 42.3542, -71.0704, 6],
-];
+//const cars: [String, Number, Number, Number][] = [
+  //["mXfkjrFw", 42.3453, -71.0464, 1],
+  //["nZXB8ZHz", 42.3662, -71.0621, 2],
+  //["Tkwu74WC", 42.3603, -71.0547, 3],
+  //["5KWpnAJN", 42.3472, -71.0802, 4],
+  //["uf5ZrXYw", 42.3663, -71.0544, 5],
+  //["VMerzMH8", 42.3542, -71.0704, 6],
+//];
 
 function setMarkers(map) {
   const image = "notuber/car.png";
-  new google.maps.Marker( {
+
+  marker = new google.maps.Marker({
     position: { lat: cars[1], lng: cars[2] },
     map,
     icon: image,
     title: cars[0],
   });
+  marker.setMap(map);
 }
 
 window.initMap = initMap;
-
-  //<script
-    //src="https://maps.googleapis.com/maps/api/js?key=APIKEY&callback=initMap&v=weekly"
-    //defer
-  //></script>
